eea802e49ce3f4e06bdf11738d03d73d898af59e
Aarav-Kartik <104106111+Aarav-Kartik@users.noreply.github.com>
2022-10-06 19:41:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("I'm dumb");
+
+var first_name = "Aarav";
+var last_name = "Kartik";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+var add = a + b ;
+console.log(add);
+var subtract = a - b ;
+console.log(subtract);
+var divide = a / b ;
+console.log(divide);
+var multiply = a * b ;
+console.log(multiply);
